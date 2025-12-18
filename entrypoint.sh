cd3f911b37e7f070c7174bd66d5c1ff403c28cda
Mark Robinson <mark.robinson@mls.uzh.ch>
2025-12-18 17:26:28 +0100

    ben's changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,15 @@
 
 # Install module dependencies using uv with the legacy Python version
 # Try mounted requirements first, fall back to baked-in version
-if [ -f /mnt/requirements-module.txt ]; then
-    uv pip install --python python-legacy --system -r /mnt/requirements-module.txt
-elif [ -f /app/requirements-module.txt ]; then
-    uv pip install --python python-legacy --system -r /app/requirements-module.txt
-fi
+INSTALL_DIR="$(pwd)/local_python_libs"
+mkdir -p "$INSTALL_DIR"
+echo "UV INSTALL..."
+uv pip install --python python-legacy -r /mnt/requirements-module.txt --target $INSTALL_DIR --no-system
 
 # Change to home directory where omnibenchmark expects output structure
-cd $HOME
+# cd $HOME
+
+export PYTHONPATH="$INSTALL_DIR:$PYTHONPATH"
 
 # Execute the module script with the legacy Python version
 # Script is expected to be mounted in /mnt (current working directory from docker run)
